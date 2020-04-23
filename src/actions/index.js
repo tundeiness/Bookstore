8757e8c5c3f4b730cc1e6711a7f0f8d0904074eb
tundeiness <tundeiness@yahoo.co.uk>
2020-04-23 21:34:37 +0100

    remove filterBook action in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,9 +8,12 @@ const removeBook = book => ({
   payload: book.id,
 });
 
-const filterBook = filter => ({
-  type: 'FILTER_BOOK',
-  filter,
-});
+// const filterBook = filter => ({
+//   type: 'FILTER_BOOK',
+//   filter,
+// });
 
-export { createBook, removeBook, filterBook };
+export { createBook, removeBook };
+
+// GET & POST /api/v1/books
+// DELETE /api/v1/books/:id
