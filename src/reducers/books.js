28ce762a264efa9948930bf7a7852c737ca7f9e1
tundeiness <tundeiness@yahoo.co.uk>
2020-05-03 13:02:29 +0100

    clean up reducer/book.js

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -19,25 +19,3 @@ const bookReducer = (state = '', action) => {
 
 
 export default bookReducer;
-
-
-// const bookReducer = (state = '', action) => {
-//   switch (action.type) {
-//     case 'CREATE_BOOK':
-//       return [
-//         ...state,
-//         action.payload,
-//       ];
-//     case 'LOAD_BOOK':
-//       return [
-//         ...state,
-//         action.payload,
-//       ];
-//     case 'REMOVE_BOOK':
-//       return state.filter(book => book.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-
-// export default bookReducer;
