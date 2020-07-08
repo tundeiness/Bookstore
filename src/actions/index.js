5969acc1a7b64cfdf42ae12f7b65d71ca14f7185
tundeiness <tundeiness@yahoo.co.uk>
2020-07-08 17:46:46 +0100

    fix lint issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,14 @@ import {
   LOAD_ALL_BOOK_REQ, CREATE_BOOK_REQ, REMOVE_BOOK_REQ, FILTER_BOOK_REQ,
 } from './actionTypes';
 
-
 const loadBooks = books => ({ type: LOAD_ALL_BOOK_REQ, books });
 
-
 const createBook = book => ({ type: CREATE_BOOK_REQ, book });
-
 
 // const removeBook = book => ({ type: REMOVE_BOOK_REQ, book });
 const removeBook = id => ({ type: REMOVE_BOOK_REQ, id });
 
 const filterBook = filter => ({ type: FILTER_BOOK_REQ, filter });
-
 
 export {
   createBook, loadBooks, removeBook, filterBook,
